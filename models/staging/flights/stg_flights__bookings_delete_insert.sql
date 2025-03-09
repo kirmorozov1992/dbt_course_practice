3a7281eac43b7bb89e46f2b40ab8a9149703beae
kirmorozov1992 <kirmorozov1992@yandex.ru>
2025-03-09 21:25:59 +0600

    add incremental update to bookings

---
+++ models/staging/flights/stg_flights__bookings_delete_insert.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+      materialized = 'incremental',
+      incremental_strategy = 'delete+insert',
+      unique_key = ['book_ref'],
+      tags = ['bookings'],
+    )
+}}
+
+select
+      book_ref,
+      book_date,
+      total_amount
+from 
+    {{ source('demo_src', 'bookings') }}
+{% if is_incremental() %}
+  where
+      book_date > (select max(book_date) from {{ source('demo_src', 'bookings') }}) - interval '7 day'
+{% endif %}
+    
